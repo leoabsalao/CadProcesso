5cb0c48065bcfefca6e8b276d810a4da726d67e8
Leonardo Absalão <100871641+leoabsalao@users.noreply.github.com>
2024-06-15 18:56:46 -0300

    Iniciado o projeto front em angular

--- back-end/serv_spring/src/main/java/com/absalao/model/Processo.java
+++ back-end/serv_spring/src/main/java/com/absalao/model/Processo.java
@@ -1,10 +1,7 @@
 package com.absalao.model;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
