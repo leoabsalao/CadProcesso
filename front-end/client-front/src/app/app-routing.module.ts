456587e27afe0b4abd3726762762c2e837e7d0d3
Leonardo Absalão <100871641+leoabsalao@users.noreply.github.com>
2024-06-16 01:02:06 -0300

    Customizando a apresentação de cores do App

--- front-end/client-front/src/app/app-routing.module.ts
+++ front-end/client-front/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'processos' }
+  { path: '', pathMatch: 'full', redirectTo: 'processos' },
+  {
+    path: 'processos',
+    loadChildren: () => import('./processos/processos.module').then(m => m.ProcessosModule)
+  }
 ];
 
 @NgModule({
